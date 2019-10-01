5eaab7246bf532513bd723bb743a8088b4765d22
Destrile <47186911+Destrile@users.noreply.github.com>
2019-10-01 16:47:17 -0400

    Helpsesh

---
+++ lab-vr/Assets/GameState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public int numSnapped = 0;
+    public GameObject WinText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerUnSnap()
+    {
+        Debug.Log("Snapped");
+        numSnapped = numSnapped - 1;
+    }
+
+    public void OnTriggerSnap()
+    {
+        Debug.Log("Snapped");
+        numSnapped = numSnapped + 1;
+        if (numSnapped == 2)
+        {
+            Debug.Log("Finished");
+            WinText.SetActive(true);
+        } else
+        {
+            Debug.Log("Not Finished");
+        }
+    }
+}
